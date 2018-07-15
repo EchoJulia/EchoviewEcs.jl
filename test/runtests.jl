a9e8633d323958e16c480a2c5cb6dfbedff13ab1
Rob Blackwell <rob@robblackwell.com>
2018-07-15 18:07:53 +0100

    Attempt to get travis testing working

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,10 @@
 using EchoviewEcs
-using Base.Test
+
+@static if VERSION < v"0.7.0-DEV.2005"
+    using Base.Test
+else
+    using Test
+end
 
 filename = joinpath(dirname(@__FILE__),
                   "data/sample.ecs")
